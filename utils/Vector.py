3671a5e8118e1553e619f7965ba16d1e5cfe0b3d
SCHWMAX <max.schwenzer@voith.com>
2024-08-02 15:33:00 +0200

    add dot product and cross product

--- utils/Vector.py
+++ utils/Vector.py
@@ -1,7 +1,7 @@
 import math
 import operator
 
-from typing import Literal, Tuple, TypeVar
+from typing import Literal, Tuple, TypeVar, Iterator
 
 from CosyTrafo import (
     cartesian_to_spherical, spherical_to_cartesian,
@@ -28,6 +28,9 @@ class Vector:
 
     def __repr__(self) -> str:
         return f"Vector({self.a}, {self.b}, {self.c}, csys={self.csys})"
+
+    def __iter__(self) -> Iterator[float]:
+        return iter((self.a, self.b, self.c))
 
     def to_cartesian(self) -> Tuple[float, float, float]:
         if self.csys == "cartesian":
@@ -72,12 +75,30 @@ class Vector:
     def __truediv__(self, other: AnyVector) -> AnyVector:
         return self.__operate(other, "truediv")
 
+    def dot(self, other: AnyVector) -> float:
+        """calculates the scalar product or dot product between two vectors"""
+        return sum(self * other)
+
+    def cross(self, other: AnyVector) -> AnyVector:
+        """calculates the cross product between two vectors"""
+        a1, b1, c1 = self.to_cartesian()
+        a2, b2, c2 = other.to_cartesian()
+
+        return Vector(
+            b1 * c2 - c1 * b2,
+            c1 * a2 - a1 * c2,
+            a1 * b2 - b1 * a2,
+            csys="cartesian"
+        )
+
 
 if __name__ == "__main__":
     vec1 = Vector(1, 2, 3, "cartesian")
     vec2 = Vector(4, 5, 6, "cartesian")
 
-    print(vec1 + vec2)
-    print(vec1 - vec2)
-    print(vec1 * vec2)
-    print(vec1 / vec2)
+    print(f"Add: {vec1 + vec2}")
+    print(f"Subtract: {vec1 - vec2}")
+    print(f"Multiply: {vec1 * vec2}")
+    print(f"Divide: {vec1 / vec2}")
+    print(f"Dot Product: {vec1.dot(vec2)}")
+    print(f"Cross Product: {vec1.cross(vec2)}")
