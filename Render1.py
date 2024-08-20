b21bef581f2116ce7995e7e2d747f5fabf4c20cb
TianXue <xuehui.tian@voith.com>
2024-08-20 14:51:07 +0200

    new comment

--- Render1.py
+++ Render1.py
@@ -26,6 +26,7 @@ def create_perpendicular_vector(
         return perp1, perp2
 
 
+#######
 def look_at(
     camera_location: Union[
         List[float], Tuple[float, float, float], NDArray[np.float64]
